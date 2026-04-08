2b23ea8887d916ded063e532d0b6a0876003056d
EduAndr3s <edu92seg@gmail.com>
2026-04-08 23:43:48 +0200

    FIX: corregido el import de Paginator en fichero AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Nette\Utils\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
