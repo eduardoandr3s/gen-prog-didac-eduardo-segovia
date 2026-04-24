9c5917e058a2c0c837b50bd039c524a00d890593
EduAndr3s <edu92seg@gmail.com>
2026-04-24 23:22:48 +0200

    enlace a listado de ciclos visible en el navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,9 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
             <a class="navbar-brand" href="/">Generador PD</a>
-            <div class="navbar-nav">
-                @auth
+              <div class="navbar-nav">
                     <a class="nav-link" href="{{ route('ciclos.index') }}">Ciclos</a>
-                    <a class="nav-link" href="{{ route('programaciones.index') }}">Programaciones</a>
-                @endauth
-            </div>
+              </div>
         </div>
     </nav>
 
